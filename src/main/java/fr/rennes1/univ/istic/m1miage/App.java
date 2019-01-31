a603990e830769e0600dd0b33b45d7f55213ce91
Maxime CHABI <maxime.chabi@kurmi-software.com>
2019-01-31 18:08:20 +0100

    Initial commit

---
+++ src/main/java/fr/rennes1/univ/istic/m1miage/App.java
@@ -0,0 +1,24 @@
+package fr.rennes1.univ.istic.m1miage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void mort(){
+        if(true){
+            System.out.println("test me");
+        }
+    }
+
+    public static void vivantMort(){
+        if(true){
+            System.out.println("test me");
+        }
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fr/rennes1/univ/istic/m1miage/AppTest.java
@@ -0,0 +1,20 @@
+package fr.rennes1.univ.istic.m1miage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
